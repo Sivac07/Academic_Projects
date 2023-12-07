e16d6087a3230fc542d9c79fdd0d408696898c2a
Siva <90086731+Sivac07@users.noreply.github.com>
2023-12-07 14:11:27 +0530

    Delete max.py
    Moving to folder

--- max.py
+++
@@ -1,7 +0,0 @@
-def maximum(x,y,z):
-    return max(x,y,z)
-x = int(input("Enter the value x: "))
-y = int(input("Enter the value y: "))
-z = int(input("Enter the value z: "))
-maximum(x,y,z)
-print(f"The maximum number is {maximum(x,y,z)} ")
