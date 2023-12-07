d310c17e82f7142e32e06ebe7c213679ac4dd33b
Siva <90086731+Sivac07@users.noreply.github.com>
2023-12-07 14:06:52 +0530

    Delete input_num.py
    Moving to folder

--- input_num.py
+++
@@ -1,8 +0,0 @@
-def num():
-    n = int(input("Enter your earnings per day in $ dollor : "))
-    if n > 100:
-        print("CONGRATS")
-    else:
-        print("HAVE A NICE DAY")      
-num()
-                
