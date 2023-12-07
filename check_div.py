5100d5c8e2411d015525d0640cc426209d4f1b4d
Siva <90086731+Sivac07@users.noreply.github.com>
2023-12-07 14:03:18 +0530

    Delete check_div.py
    Moving to folder

--- check_div.py
+++
@@ -1,5 +0,0 @@
-number = int(input("Enter a number: "))
-if number % 2 == 0 and number % 3 == 0:
-    print(f"{number} is divisible by both 2 and 3.")
-else:
-    print(f"{number} is not divisible by both 2 and 3.")
