a3920a614ce314abceb705205fd5b60c6cfeeeaa
Siva <90086731+Sivac07@users.noreply.github.com>
2023-12-07 14:14:27 +0530

    Delete string_print.py
    Moving to folder

--- string_print.py
+++
@@ -1,6 +0,0 @@
-def product():
-    product_name = input("Enter the Product Name: ")
-    product_code = input("Enter the Product Code: ")
-    price = int(input("Enter the product price: "))
-    print("The " + product_name + " with " + product_code + " is sold as Rs." + str(price))
-product()
