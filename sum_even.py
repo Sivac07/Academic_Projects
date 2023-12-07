52f453be2c0153f02bec6c9c30250c1e378f9763
Siva <90086731+Sivac07@users.noreply.github.com>
2023-12-07 14:14:52 +0530

    Delete sum_even.py
    Moving to folder

--- sum_even.py
+++
@@ -1,7 +0,0 @@
-def sum_even():
-    sum_of_evens = 0
-    for num in range(100,201):
-        if num%2 == 0:
-            sum_of_evens += num
-    print("Sum of even numbers between 100 and 200: ", sum_of_evens)
-sum_even()
