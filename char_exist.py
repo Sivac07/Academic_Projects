9337c87eaba35b45e363d8170231abe38d4864c7
Siva <90086731+Sivac07@users.noreply.github.com>
2023-12-07 14:02:55 +0530

    Delete char_exist.py
    Moving to folder

--- char_exist.py
+++
@@ -1,8 +0,0 @@
-def exist():
-    name = "JOSEPH"
-    a = input("Enter the character to check: ")
-    if a in name:
-        print(f"The character {a} is in name")
-    else:
-        print(f"The character {a} is not in name")
-exist()
